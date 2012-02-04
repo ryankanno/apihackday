0360079093e458894cc3d11c5fab282a4c2f9800
Ryan Kanno <ryankanno@localkinegrinds.com>
2012-02-04 10:42:16 -0500

    Added empty base template

--- apps/fans/views/www_views.py
+++ apps/fans/views/www_views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render_to_response
 def slash(request):
     return render_to_response('www/slash.html', {}, 
         context_instance=RequestContext(request))
+
+
+def update(request):
+    return render_to_response('www/slash.html', {}, 
+        context_instance=RequestContext(request))
