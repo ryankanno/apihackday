182045643a60c8cfcdbf9e13f609ba8633d8f930
Ryan Kanno <ryankanno@localkinegrinds.com>
2012-02-04 11:22:18 -0500

    Added cache decorator Added dogpile Added slash

--- apps/fans/views/www_views.py
+++ apps/fans/views/www_views.py
@@ -1,12 +1,27 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response 
+from django.conf import settings
 
+from fanfeedr import FanFeedrAPI
+from utilities.cache.decorators import cacheable
+
+
+FANFEEDR_API_KEY = getattr(settings, 'FANFEEDR_API_KEY', None)
+
+#"http://ffapi.fanfeedr.com/basic/api/leagues/f65226d8-fbf7-5033-a7a0-50de55b57968/events?api_key=mykey
 
 def slash(request):
-    return render_to_response('www/slash.html', {}, 
+    leagues = get_leagues()
+    return render_to_response('www/slash.html', {'leagues': leagues}, 
         context_instance=RequestContext(request))
 
 
 def update(request):
     return render_to_response('www/slash.html', {}, 
         context_instance=RequestContext(request))
+
+
+@cacheable(ttl=60)
+def get_leagues():
+    api = FanFeedrAPI(FANFEEDR_API_KEY, tier="basic") 
+    return api.get_collection("leagues")
---
+++ utilities/cache/decorators.py
@@ -0,0 +1,46 @@
+import os
+import settings
+from types import FunctionType
+from functools import wraps
+
+from django.core.cache import cache
+from utilities.cache.dogpile import cache_get, cache_set
+
+CURRENT_VERSION = getattr(settings, '', '')
+
+def cacheable(key=None, ttl=60, is_enabled=True):
+    """
+    Decorator for cacheable function
+    """
+    def decorator(fxn):
+        def arg2str(arg):
+            if type(arg) == FunctionType:
+                return arg.__name__
+            else:
+                return str(arg)
+
+        if callable(key):
+            key_fun = key
+        else:
+            if key is None:
+                key_fun = lambda *args, **kwargs: '%s-%s-%s' % \
+                    (fxn.__module__, fxn.__name__, '-'.join(map(arg2str, args)))
+            else:
+                key_fun = lambda *args, **kwargs: key % args[:key.count('%')]
+        
+        @wraps(fxn)
+        def wrapper(*args, **kwargs):
+            if is_enabled:
+                key = key_fun(*args, **kwargs)
+                versioned_key = "%s-%s" % (CURRENT_VERSION, key)
+                data = cache_get(cache, versioned_key)
+
+                if data is None:
+                    data = fxn(*args, **kwargs)
+                    cache_set(cache, versioned_key, data, ttl)
+                return data
+            else:
+                return fxn(*args, **kwargs)
+
+        return wrapper
+    return decorator 
