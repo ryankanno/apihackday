7c09eb74cae1aa5d5c13ee45dc53ea6fc615f19b
Ryan Kanno <ryankanno@localkinegrinds.com>
2012-03-04 18:22:03 -0500

    Added None check

--- utilities/context_processors.py
+++ utilities/context_processors.py
@@ -6,13 +6,12 @@ EXPOSED_SETTINGS = getattr(conf_settings, 'EXPOSED_SETTINGS', None)
 
 def settings(request):
     exposed = {}
-    for x in EXPOSED_SETTINGS:
-        exposed[x] = getattr(conf_settings, x)
+    if EXPOSED_SETTINGS:
+        for x in EXPOSED_SETTINGS:
+            exposed[x] = getattr(conf_settings, x)
     return { 'settings': exposed }
 
 
 def module_view(request):
     return {'module_name': getattr(request, 'module_name', '').replace('.','-'),
             'view_name'  : getattr(request, 'view_name', '').replace('.','-')} 
-    
-
