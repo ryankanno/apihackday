780616b01496c176c27f95d3274420181f461951
Ryan Kanno <ryankanno@localkinegrinds.com>
2012-03-04 14:42:15 -0500

    Updated TIER to FANFEEDR_TIER

--- apps/fans/utils/fanfeedr_helper.py
+++ apps/fans/utils/fanfeedr_helper.py
@@ -8,7 +8,7 @@ import logging
 LOG = logging.getLogger(__name__)
 
 FANFEEDR_API_KEY = getattr(settings, 'FANFEEDR_API_KEY', None)
-TIER             = getattr(settings, 'TIER', None)
+FANFEED_TIER     = getattr(settings, 'FANFEEDR_TIER', None)
 VERSION          = "1"
 
 YEAR_CACHE = 60*60*24*365
@@ -17,7 +17,7 @@ YEAR_CACHE = 60*60*24*365
 @cacheable(ttl=YEAR_CACHE)
 def get_leagues(version=VERSION):
     LOG.info("Calling get_leagues")
-    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=TIER) 
+    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=FANFEEDR_TIER) 
     try:
         leagues = api.get_collection("leagues")
     except Exception as e:
@@ -28,14 +28,14 @@ def get_leagues(version=VERSION):
 
 @cacheable(ttl=60*60*4)
 def get_upcoming_games(version=VERSION, id=None):
-    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=TIER) 
+    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=FANFEEDR_TIER) 
     return api.get_collection_method("next", "events", ptype="leagues", puid=id)
 
 
 @cacheable(ttl=YEAR_CACHE)
 def get_previous_games(version=VERSION, id=None):
     LOG.info("Calling get_previous_games")
-    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=TIER) 
+    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=FANFEEDR_TIER) 
     try:
         games = api.get_collection_method("last", "events", ptype="leagues", puid=id)
     except Exception as e:
@@ -46,14 +46,14 @@ def get_previous_games(version=VERSION, id=None):
 
 @cacheable(ttl=60*60*1)
 def get_todays_games(version=VERSION, id=None):
-    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=TIER) 
+    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=FANFEEDR_TIER) 
     return api.get_collection_method("today", "events", ptype="leagues", puid=id)
 
 
 @cacheable(ttl=60*60*4)
 def get_event(version=VERSION, id=None):
     LOG.info("Calling get_event")
-    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=TIER) 
+    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=FANFEEDR_TIER) 
     try:
         evt = api.get_resource("events", id)
     except Exception as e:
@@ -65,7 +65,7 @@ def get_event(version=VERSION, id=None):
 @cacheable(ttl=YEAR_CACHE)
 def get_boxscore(version=VERSION, id=None):
     LOG.info("Calling get_boxscore")
-    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=TIER) 
+    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=FANFEEDR_TIER) 
     try:
         bs = api.get_collection("boxscore", "events", id)
     except Exception as e:
@@ -76,13 +76,13 @@ def get_boxscore(version=VERSION, id=None):
 
 def get_lineup(version=VERSION, id=None):
     LOG.info("Calling get_lineup")
-    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=TIER) 
+    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=FANFEEDR_TIER) 
     return api.get_collection("lineup", "events", id)
 
 
 def get_recap(version=VERSION, id=None):
     LOG.info("Calling get_recap for game(id={0})".format(id))
-    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=TIER) 
+    api = FanFeedrAPI(FANFEEDR_API_KEY, tier=FANFEEDR_TIER) 
     try:
         recap = api.get_collection("recap", "events", id)
     except Exception as e:
